9376ee5b19e5236eb81bf3ebbba1273bc19a4667
Mohit Tyagi <mohit.tyagi.de@gmail.com>
2025-08-11 22:40:54 +0200

    adding classes for demostration

---
+++ ConsoleAppPS/ConsoleAppPS/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPS
+{
+    public class BankAccount
+    {   
+       // public event Action BankAccountCreatedEvent;
+
+        public BankAccount() { }
+
+        public void BankAccountCreated()
+        {
+            Console.WriteLine( "Bank account created successfully.");
+        }
+
+    }
+}
--- ConsoleAppPS/ConsoleAppPS/Program.cs
+++ ConsoleAppPS/ConsoleAppPS/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppPS;
+
 Console.WriteLine("Hello, World!");
+
+
+ExpatUser user = new ExpatUser();
+BankAccount bankAccount = new BankAccount();
+Registration registration = new Registration();
+
+user.UserCreatedEvent += bankAccount.BankAccountCreated;
+user.UserCreatedEvent += registration.RegisterUser;
+
+user.UserCreatedMethod();
+
+
+
+Console.ReadLine();
